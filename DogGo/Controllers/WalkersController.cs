ea4547299d2a10b7501e4e1b9a3c46b45798977c
Jackson <jacksonrgoodman@gmail.com>
2021-06-21 11:47:12 -0500

    initial commit

---
+++ DogGo/Controllers/WalkersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogGo.Controllers
+{
+    public class WalkersController : Controller
+    {
+        private readonly IWalkerRepository _walkerRepo;
+
+        // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
+        public WalkersController(IWalkerRepository walkerRepository)
+        {
+            _walkerRepo = walkerRepository;
+        }
+        //TODO GET: Walkers
+        public ActionResult Index()
+        {
+            List<Walker> walkers = _walkerRepo.GetAllWalkers();
+
+            return View(walkers);
+        }
+        //TODO GET: Walkers/Details/*example ID*
+        public ActionResult Details(int id)
+        {
+            Walker walker = _walkerRepo.GetWalkerById(id);
+
+            if (walker == null)
+            {
+                return NotFound();
+            }
+
+            return View(walker);
+        }
+
+    }
+}
